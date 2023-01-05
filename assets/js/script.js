b7854c39858b2281c9d295ab85c7e697ab84656a
m29mujib <mamujib29@gmail.com>
2023-01-05 20:29:23 +0700

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+ AOS.init({
+     once: true,
+ });
+
+ var navlink = document.getElementById("navlink");
+
+ function showMenu() {
+     navlink.style.right = "0";
+ }
+
+ function hideMenu() {
+     navlink.style.right = " -200px";
+ }
